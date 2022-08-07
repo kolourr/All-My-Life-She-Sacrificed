32cf970c77578a427e2b4202d6044307e47ab598
Bruce <contact@estimatebodyfat.com>
2022-08-07 00:12:55 -0400

    update

--- models/post.js
+++ models/post.js
@@ -33,7 +33,7 @@ const postSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment',
     }],  
-    createdAt: {
+    createdAt: {  
         type: Date,
         default: Date.now,
       },
