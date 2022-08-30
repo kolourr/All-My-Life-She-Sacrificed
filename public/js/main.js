cde540243a7b6b40f41b315355aab77abfe0e094
Bruce <contact@estimatebodyfat.com>
2022-08-30 19:08:38 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,3 @@
-//Cropping Profile Picture using Croppie
-
-//initialize Croppie
- 
- let vanilla =  new Croppie(document.getElementById('image_demo'), {
-        enableExif: true,
-        showZoomer: true,
-        enableOrientation: true,
-        viewport: {
-            width:200, 
-            height:200,
-            type:'circle' //circle
-        },
-        boundary:{
-            width:300,
-            height:300 
-        }
-    })
-
-
-
-document.getElementById('upload_image').addEventListener('change', displayImage)
-//Display Image
- function displayImage(){
-    let reader =  new FileReader();
-    reader.onload = function (event) {
-        vanilla.bind({
-        url: event.target.result
-      }) 
-    }
-    reader.readAsDataURL(this.files[0]); 
-    document.getElementById('uploadimage').style.display = 'block' 
-
-}
-
-document.getElementById('crop_image').addEventListener('click', uploadBase64)
-//Upload base64 of Cropped Image for Server Side rendering and upload to S3
-
- function uploadBase64(){
-    vanilla.result({
-        type: 'base64',
-        size: 'viewport'
-      }).then( function(response){
-               fetch('/editProfilePicture', {
-                  method: 'post',
-                  headers: {'Content-type': 'application/json'},
-                  body: JSON.stringify({
-                      'base64': response
-                  })
-  
-              })
-              location.reload()
-  
-      })
-}
-
-
- 
-
 //Adding and removing hearts, and deleting posts/comments 
 
 const deletePostButton = document.querySelectorAll('.deletePost')
@@ -205,4 +146,62 @@ async function commentHeartBreakIncreaseDecrease(){
     }
 
 }
+
+//Cropping Profile Picture using Croppie
+
+//initialize Croppie
+ 
+let vanilla =  new Croppie(document.getElementById('image_demo'), {
+    enableExif: true,
+    showZoomer: true,
+    enableOrientation: true,
+    viewport: {
+        width:200, 
+        height:200,
+        type:'circle' //circle
+    },
+    boundary:{
+        width:300,
+        height:300 
+    }
+})
+
+
+
+document.getElementById('upload_image').addEventListener('change', displayImage)
+//Display Image
+function displayImage(){
+let reader =  new FileReader();
+reader.onload = function (event) {
+    vanilla.bind({
+    url: event.target.result
+  }) 
+}
+reader.readAsDataURL(this.files[0]); 
+document.getElementById('uploadimage').style.display = 'block' 
+
+}
+
+document.getElementById('crop_image').addEventListener('click', uploadBase64)
+//Upload base64 of Cropped Image for Server Side rendering and upload to S3
+
+function uploadBase64(){
+vanilla.result({
+    type: 'base64',
+    size: 'viewport'
+  }).then( function(response){
+           fetch('/editProfilePicture', {
+              method: 'post',
+              headers: {'Content-type': 'application/json'},
+              body: JSON.stringify({
+                  'base64': response
+              })
+
+          })
+          location.reload()
+
+  })
+}
+
+
 
