7374c621f931d64a9182ac02fff692f57e0540a7
Bruce <contact@estimatebodyfat.com>
2022-09-30 09:55:07 -0400

    update 2

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.get(
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     res.redirect('/dashboard')
-  }
+  }   
 )
 
 router.get('/logout', (req, res) => {
