13281889e15e6f5622738883c186f2582d3f4036
Bruce <contact@estimatebodyfat.com>
2022-10-04 18:03:40 -0400

    edit passport file

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "https://www.allmylifeshesacrificed.com/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
